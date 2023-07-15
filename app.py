c50cbf3fb4dd672a82189fd862a894501fbceef1
Cyril Sebastian p <81683894+Cyril-19@users.noreply.github.com>
2023-07-15 12:02:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,20 +3,23 @@ from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
 
-@app.route("/sms", methods=['POST'])
-def sms_reply():
-    """Respond to incoming calls with a simple text message."""
-    # Fetch the message
-    msg = request.form.get('Body')
 
-    # Create reply
-    resp = MessagingResponse()
-    resp.message("You said: {}".format(msg))
-
+@app.route("/incoming", methods=['POST'])
+def incoming():
+    body=request.values.get("Body","").lower()
+    resp=MessagingResponse()
+    if body =="hi":
+    	resp.message("Hi! I'm here to help you.Please make a choice :\n1. New voter\n2.Existing voter")
+    elif body=="1":
+    	resp.message("you chose New voter.Redirecting you to the New Voter registration website.")
+    	resp.redirect("https://voters.eci.gov.in")
+    elif body =="2":
+    	resp.message("yoi chose  existing voter.Redirecting you to Existing voter login website.")
+    	resp.redirect("https://electorlsearch.eci.gov.in")
+    else:
+    	resp.message("invalid choice.Please enter  for new voter or  for existing voter.")
+  
     return str(resp)
 
 if __name__ == "__main__":
