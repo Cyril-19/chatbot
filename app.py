fc66653a600e47b97d6a560400a9084a5438e261
Cyril Sebastian p <81683894+Cyril-19@users.noreply.github.com>
2023-07-15 12:03:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
-
 app = Flask(__name__)
-
-
-
 @app.route("/incoming", methods=['POST'])
 def incoming():
     body=request.values.get("Body","").lower()
