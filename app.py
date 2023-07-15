4af7b5bafc9178382f7ad50ecbf38cb32a5bb43d
Cyril Sebastian p <81683894+Cyril-19@users.noreply.github.com>
2023-07-15 13:19:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+sagefrom flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 app = Flask(__name__)
 @app.route("/incoming", methods=['POST'])
@@ -9,10 +9,10 @@ def incoming():
     	resp.message("Hi! I'm here to help you.Please make a choice :\n1. New voter\n2.Existing voter")
     elif body=="1":
     	resp.message("you chose New voter.Redirecting you to the New Voter registration website.")
-    	resp.redirect("https://voters.eci.gov.in")
+    	resp.message("https://voters.eci.gov.in")
     elif body =="2":
     	resp.message("yoi chose  existing voter.Redirecting you to Existing voter login website.")
-    	resp.redirect("https://electorlsearch.eci.gov.in")
+    	resp.message("https://electorlsearch.eci.gov.in")
     else:
     	resp.message("invalid choice.Please enter  for new voter or  for existing voter.")
   
